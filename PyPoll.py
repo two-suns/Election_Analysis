1f5befdf7a9593f601381759989a4d3442afd7a9
Bryan Eckard <ch13f_48@zoho.com>
2022-01-17 18:55:50 -0500

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -23,6 +23,11 @@ candidate_options = []
 # 1. Create empty dictionary
 candidate_votes = {}
 
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
@@ -50,12 +55,29 @@ with open(file_to_load) as election_data:
         # Add a vote to that candidate's count.
         candidate_votes[candidate_name] += 1
 
+# Determine the percentage of votes for each candidate by looping through the counts.
+# Iterate through the candidate list.
 for candidate_name in candidate_votes:
     # Retrieve vote count of a candidate.
     votes = candidate_votes[candidate_name]
     # Calculate the percentage of votes.
     vote_percentage = float(votes) / float(total_votes) * 100
-    print(f"{candidate_name}: received {vote_percentage: .1f}% of the vote.")
 
-# Print the total_votes.
-print(candidate_votes)
+# To do: print out each candidate's name, vote count, and percentage of votes to the terminal.
+    print(f"{candidate_name}: {vote_percentage: .1f}% ({votes:,})\n")
+
+# Determine winning vote count and candidate
+# Determine if the votes are greater than the winning count.
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        # If true then set winning_count = votes and winning_percent = vote_percentage
+        winning_count = votes
+        winning_percentage = vote_percentage
+        # Set the winning_candidate equal to the candidate's name.
+        winning_candidate = candidate_name
+    
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Percentage: {winning_percentage: .1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
