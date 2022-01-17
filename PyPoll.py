ac41e0a420490fe511c4f16a7892e9935c59b207
Bryan Eckard <ch13f_48@zoho.com>
2022-01-16 20:14:04 -0500

    Updated with vote count

--- PyPoll.py
+++ PyPoll.py
@@ -15,6 +15,14 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# Initialize a total vote counter.
+total_votes = 0
+
+candidate_options = []
+
+# 1. Create empty dictionary
+candidate_votes = {}
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
@@ -23,4 +31,31 @@ with open(file_to_load) as election_data:
 
     # Print the header row.
     headers = next(file_reader)
-    print(headers)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        # Add to the total vote count.
+        total_votes += 1
+
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+
+        if candidate_name not in candidate_options:
+            # Add it to the list of candidates
+            candidate_options.append(candidate_name)
+
+            # 2. Begin tracking that candidate's vote count
+            candidate_votes[candidate_name] = 0
+
+        # Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1
+
+for candidate_name in candidate_votes:
+    # Retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+    # Calculate the percentage of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+    print(f"{candidate_name}: received {vote_percentage: .1f}% of the vote.")
+
+# Print the total_votes.
+print(candidate_votes)
